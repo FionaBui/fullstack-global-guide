a2f2604d8b3c14c7b57527f11750587dae4f4ef6
Fiona Bui <buiphf@gmail.com>
2025-05-03 17:34:44 +0200

    Complete database.ts with create & insert logic

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,18 +1,20 @@
 import sqlite3 from "sqlite3";
-import { open } from "sqlite";
-import { Database } from "sqlite";
+import { open, Database } from "sqlite";
+import { countries } from "../data/countries";
+import type { Country } from "../types/Country";
 
 let db: Database
 
 export const getDb = async ()=> {
     if(!db){
         db = await open({
-            filename: '.countries.sqlite',
+            filename: 'countries.sqlite',
             driver: sqlite3.Database
         })
 
         await db.run('PRAGMA foreign_keys = ON')
 
+        // Create countries table if it doesn't exist
         await db.run(`
             CREATE TABLE IF NOT EXISTS countries (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -28,6 +30,32 @@ export const getDb = async ()=> {
             borders TEXT,
             cca3 TEXT UNIQUE
             )`)
+
+        //Check if the table has no data → insert sample data
+        const result = await db.get<{count : number }>('SELECT COUNT(*) as count FROM countries')
+        if (result?.count === 0){
+          for (const country of countries){
+            await db.run(`
+              INSERT INTO countries
+              (name, capital, region, subregion,population, flag, languages, currencies, timezones, borders, cca3)
+              VALUES (?,?,?,?,?,?,?,?,?,?,?)
+              ` ,[
+                country.name,
+                country.capital,
+                country.region,
+                country.subregion,
+                country.population,
+                country.flag,
+                JSON.stringify(country.languages),
+                JSON.stringify(country.currencies),
+                JSON.stringify(country.timezones),
+                JSON.stringify(country.borders),
+                country.cca3
+              ]
+            )
+          }
+          console.log("Sample countries inserted into database");
+        }
     }
     return db
 }
---
+++ src/types/Country.ts
@@ -0,0 +1,14 @@
+export type Country = {
+    name: string
+    capital: string
+    region: string
+    subregion: string
+    population: number
+    flag: string
+    languages: string[]
+    currencies: string[]
+    timezones: string[]
+    borders: string[]
+    cca3: string
+  }
+  
